f2832c644fbb1f5d130810ab3847739d0ed15600
David Matheson <mathesond2@gmail.com>
2021-12-20 09:20:38 -0500

    add fix for local dev

--- models/base.go
+++ models/base.go
@@ -6,18 +6,29 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
 
 func GetDB() *gorm.DB {
+	e := godotenv.Load()
+	if e != nil {
+		fmt.Print(e)
+	}
+
 	username := os.Getenv("db_user")
 	password := os.Getenv("db_pass")
 	dbName := os.Getenv("db_name")
 	dbHost := os.Getenv("db_host")
 	dbPort := os.Getenv("db_port")
 
-	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=require password=%s", dbHost, dbPort, username, dbName, password)
-	fmt.Println(dbUri, "dave")
+	var sslMode string
+	if os.Getenv("db_host") == "localhost" {
+		sslMode = "disable"
+	} else {
+		sslMode = "require"
+	}
 
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s password=%s", dbHost, dbPort, username, dbName, sslMode, password)
 	db, err := gorm.Open("postgres", dbUri)
 	if err != nil {
 		fmt.Print(err)
