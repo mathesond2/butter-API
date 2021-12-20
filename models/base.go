58dc0b563e77e3a4d2c704d92f2910ec3fa9d790
David Matheson <mathesond2@gmail.com>
2021-12-19 19:19:36 -0500

    w

--- models/base.go
+++ models/base.go
@@ -9,18 +9,13 @@ import (
 )
 
 func GetDB() *gorm.DB {
-	// e := godotenv.Load()
-	// if e != nil {
-	// 	fmt.Print(e)
-	// }
-
 	username := os.Getenv("db_user")
 	password := os.Getenv("db_pass")
 	dbName := os.Getenv("db_name")
 	dbHost := os.Getenv("db_host")
 	dbPort := os.Getenv("db_port")
 
-	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=reuquire password=%s", dbHost, dbPort, username, dbName, password)
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=require password=%s", dbHost, dbPort, username, dbName, password)
 	fmt.Println(dbUri, "dave")
 
 	db, err := gorm.Open("postgres", dbUri)
