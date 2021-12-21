376004c53be38c0db0aaa17f4db8a86069f323f6
David Matheson <mathesond2@gmail.com>
2021-12-20 22:32:18 -0500

    wip, done for the day

--- controllers/webhooksControllers.go
+++ controllers/webhooksControllers.go
@@ -1,13 +1,12 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"go-invoices/models"
 	u "go-invoices/utils"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 )
 
 var GetTxn = func(w http.ResponseWriter, r *http.Request) {
@@ -15,38 +14,77 @@ var GetTxn = func(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(txn)
 	if err != nil {
-		fmt.Println(err, "zzz")
+		fmt.Println(err)
 		u.Respond(w, u.Message(false, "GetTxn: Error while decoding request body"))
 		return
 	}
 
 	fmt.Print(string(txn.WatchedAddress))
 
-	blah := url.Values{
-		"asset":          {txn.Asset},
-		"status":         {txn.Status},
-		"from":           {txn.From},
-		"to":             {txn.To},
-		"watchedAddress": {txn.WatchedAddress},
-		"value":          {txn.Value},
-		"direction":      {txn.Direction},
+	// blah := url.Values{
+	// 	"asset":          {txn.Asset},
+	// 	"status":         {txn.Status},
+	// 	"from":           {txn.From},
+	// 	"to":             {txn.To},
+	// 	"watchedAddress": {txn.WatchedAddress},
+	// 	"value":          {txn.Value},
+	// 	"direction":      {txn.Direction},
+	// }
+
+	params := make(map[string]interface{})
+	params["asset"] = txn.Asset
+	params["status"] = txn.Status
+	params["from"] = txn.From
+	params["to"] = txn.To
+	params["watchedAddress"] = txn.WatchedAddress
+	params["value"] = txn.Value
+	params["direction"] = txn.Direction
+
+	bytesData, _ := json.Marshal(params)
+	reader := bytes.NewReader(bytesData)
+
+	// data, err := http.PostForm("http://stormy-cove-04196.herokuapp.com/api/associatedTxn", blah)
+	request, error := http.NewRequest(
+		http.MethodPost,
+		"http://stormy-cove-04196.herokuapp.com/api/associatedTxn",
+		reader,
+		// strings.NewReader(blah.Encode()),
+	)
+	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+	if error != nil {
+		fmt.Println("Error is req: ", error)
 	}
 
-	data, err := http.PostForm("http://stormy-cove-04196.herokuapp.com/api/associatedTxn", blah)
-	if err != nil {
-		fmt.Println("Error is req: ", err)
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
 	}
+	defer response.Body.Close()
 
-	defer data.Body.Close()
-	body, err := ioutil.ReadAll(data.Body)
-	if err != nil {
-		print("read body error: ", err)
+	invoice := &models.Invoice{}
+	decodeInvoiceErr := json.NewDecoder(response.Body).Decode(invoice)
+	if decodeInvoiceErr != nil {
+		fmt.Println(decodeInvoiceErr)
+		u.Respond(w, u.Message(false, "GetAssociatedTxn: Error while decoding request body"))
+		return
 	}
+	// body, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	print("read body error: ", err)
+	// }
 
-	fmt.Print(string(body))
-	fmt.Println("data	", data)
+	// fmt.Println("response Body:", string(body))
 
+	// defer data.Body.Close()
+	// body, err := ioutil.ReadAll(data.Body)
+	// if err != nil {
+	// 	print("read body error: ", err)
+	// }
+
+	// fmt.Print(string(body))
+	// ioutil.ReadAll
 	resp := u.Message(true, "success")
-	resp["data"] = data
+	resp["data"] = invoice
 	u.Respond(w, resp)
 }
