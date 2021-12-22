036923d1e22c6dacc686e7c9f540d7d7f67e8937
David Matheson <mathesond2@gmail.com>
2021-12-22 16:54:48 -0500

    adds webhook controller/model files

--- controllers/TxnsControllers.go
+++ controllers/TxnsControllers.go
@@ -8,9 +8,7 @@ import (
 	u "go-invoices/utils"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strconv"
-	"strings"
 )
 
 var ParseMempoolEvent = func(w http.ResponseWriter, r *http.Request) {
@@ -87,111 +85,5 @@ var GetAssociatedTxn = func(w http.ResponseWriter, r *http.Request) {
 
 	resp := u.Message(true, "success")
 	resp["data"] = data
-	u.Respond(w, resp)
-}
-
-type PostBody struct {
-	ApiKey     string   `json:"apiKey"`
-	Address    string   `json:"address"`
-	Blockchain string   `json:"blockchain"`
-	Networks   []string `json:"networks"`
-}
-
-var AddWallet = func(w http.ResponseWriter, r *http.Request) {
-	addressAuth := &models.AddressAuth{}
-
-	err := json.NewDecoder(r.Body).Decode(addressAuth)
-	if err != nil {
-		fmt.Println(err)
-		u.Respond(w, u.Message(false, "Invalid request"))
-		return
-	}
-
-	if len(addressAuth.Addresses) == 0 {
-		u.Respond(w, u.Message(false, "Invalid request: no addresses provided"))
-		return
-	}
-
-	// var resp models.Response
-
-	var addressReqResults = make(map[string]string)
-	//this should be its own validate fn where we also look up any dupes in the db
-	for _, address := range addressAuth.Addresses {
-		if !strings.HasPrefix(address, "0x") {
-			u.Respond(w, u.Message(false, "only valid Ethereum addresses are currently accepted"))
-			return
-		}
-	}
-
-	for _, address := range addressAuth.Addresses {
-		supportedNetworks := []string{
-			"main",
-			"rinkeby",
-		}
-
-		postBody := PostBody{
-			os.Getenv("blocknative_api_key"),
-			address,
-			"ethereum",
-			supportedNetworks,
-		}
-
-		jsonData, jsonErr := json.Marshal(postBody)
-		if jsonErr != nil {
-			fmt.Println("error: ", jsonErr)
-		}
-
-		resp, httpErr := http.Post(
-			"https://api.blocknative.com/address",
-			"application/json; charset=utf-8",
-			bytes.NewBuffer(jsonData),
-		)
-
-		if httpErr != nil {
-			fmt.Println("watch address Error: ", httpErr)
-		}
-
-		defer resp.Body.Close()
-		body, ioErr := ioutil.ReadAll(resp.Body)
-		if ioErr != nil {
-			fmt.Println("ioErr: ", ioErr)
-		}
-
-		type Response struct {
-			Msg string `json:"msg"`
-		}
-		var res Response
-		json.Unmarshal(body, &res)
-		addressReqResults[address] = res.Msg
-	}
-
-	resp := u.Message(true, "success")
-	resp["data"] = addressReqResults
-	u.Respond(w, resp)
-}
-
-var AddWebhook = func(w http.ResponseWriter, r *http.Request) {
-	user := r.Context().Value("user").(uint)
-	webhook := &models.PreParsedWebhook{}
-
-	err := json.NewDecoder(r.Body).Decode(webhook)
-	if err != nil {
-		fmt.Println(err)
-		u.Respond(w, u.Message(false, "addWebhook: Error while decoding request body"))
-		return
-	}
-
-	webhook.UserId = user
-	hackyArrToStr := strings.Join(webhook.Networks, " ")
-
-	parsedWebhook := &models.Webhook{
-		Address:      webhook.Address,
-		Networks:     hackyArrToStr,
-		Name:         webhook.Name,
-		Endpoint_Url: webhook.Endpoint_Url,
-		UserId:       user,
-	}
-
-	resp := models.CreateWebhook(parsedWebhook)
 	u.Respond(w, resp)
 }
--- models/txn.go
+++ models/txn.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"fmt"
-	u "go-invoices/utils"
 
 	"github.com/jinzhu/gorm"
 )
@@ -57,56 +56,4 @@ func GetAssociatedTxn(txn *ParsedTransaction) *Invoice {
 	}
 
 	return invoice
-}
-
-type PreParsedWebhook struct {
-	Address      string   `json:"address"`
-	Networks     []string `json:"networks"`
-	Name         string   `json:"name"`
-	Endpoint_Url string   `json:"endpoint_url"`
-	UserId       uint     `json:"user_id"`
-}
-type Webhook struct {
-	gorm.Model
-	Address      string `json:"address"`
-	Networks     string `json:"networks"`
-	Name         string `json:"name"`
-	Endpoint_Url string `json:"endpoint_url"`
-	UserId       uint   `json:"user_id"`
-}
-
-func (webhook *Webhook) ValidateWebhook() (map[string]interface{}, bool) {
-	if webhook.Address == "" {
-		return u.Message(false, "address should be on the payload"), false
-	}
-
-	if webhook.Networks == "" {
-		return u.Message(false, "chosen networks should be on the payload"), false
-	}
-
-	if webhook.Name == "" {
-		return u.Message(false, "webhook name should be on the payload"), false
-	}
-
-	if webhook.Endpoint_Url == "" {
-		return u.Message(false, "endpoint_url should be on the payload"), false
-	}
-
-	if webhook.UserId <= 0 {
-		return u.Message(false, "User is not recognized"), false
-	}
-
-	return u.Message(true, "success"), true
-}
-
-func CreateWebhook(webhook *Webhook) map[string]interface{} {
-	if resp, ok := webhook.ValidateWebhook(); !ok {
-		return resp
-	}
-
-	GetDB().Create(webhook)
-
-	resp := u.Message(true, "success")
-	resp["data"] = webhook
-	return resp
 }
