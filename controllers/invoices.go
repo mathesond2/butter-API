fba06c256565f730451a02b29d44a37b089d7cc6
David Matheson <mathesond2@gmail.com>
2021-12-23 09:28:12 -0500

    re-add controller dir

---
+++ controllers/invoices.go
@@ -0,0 +1,72 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-invoices/models"
+	u "go-invoices/utils"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+var CreateInvoice = func(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(uint) //Grab the id of the user that send the request
+	invoice := &models.Invoice{}
+
+	err := json.NewDecoder(r.Body).Decode(invoice)
+	if err != nil {
+		fmt.Println(err)
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+
+	invoice.UserId = user
+	resp := invoice.CreateInvoice()
+	u.Respond(w, resp)
+}
+
+var UpdateInvoice = func(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(uint)
+	vars := mux.Vars(r)
+	id, _ := strconv.ParseUint(vars["id"], 10, 32)
+
+	invoice := &models.Invoice{}
+
+	err := json.NewDecoder(r.Body).Decode(invoice)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+
+	invoice.UserId = user
+	resp := models.UpdateInvoice(id, invoice)
+	u.Respond(w, resp)
+}
+
+var DeleteInvoice = func(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.ParseUint(vars["id"], 10, 32)
+	data := models.DeleteInvoice(id)
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
+
+var GetInvoice = func(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.ParseUint(vars["id"], 10, 32)
+	data := models.GetInvoice(id)
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
+
+var GetInvoices = func(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value("user").(uint64)
+	data := models.GetInvoices(id)
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
